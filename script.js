a5e50eed7b720deaaef6904fcadb6a91fcde614d
vishnuvardhan481999 <160810215+vishnuvardhan481999@users.noreply.github.com>
2024-03-11 12:54:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+let resume = {
+    "basics":{
+        "name": "vishnuvardhan m",
+        "email": "vishnuvardhan481999@gmail.com",
+        "phone": 8124250027,
+        "degree": "B.E",
+        "branch":"MECH",
+        "locations":{
+            "address":"11/16,T.nanage 3rd st,ramanathapuram,coimbatore",
+            "pincode":641045,
+            "city":"Coimbatore",
+            "country":"India"
+        },
+        "profiles":[
+            {
+            "website": "https://www.linkedin.com/in/vishnu-vardhanv45y3y343/",
+            "github": "https://github.com/VishnuvardhanM",
+            }
+        ]
+
+    },
+    "work":[
+        {
+            "company":"Cognizant Technology Services",
+            "position":"Senior systems Engineer",
+            "start date":"5-1-2022 to present"
+        },
+    ],
+    "education":[
+        {
+            "institute":"Sri Shakthi Institute of Engg and technology",
+            "department":"Mech",
+            "batch start year":"2017",
+            "batch end year":"2021",
+            "CGPA":7.2,
+        }
+
+    ],
+    "skills":
+    [
+        {
+        "name":"JS,HTML,CSS,SQL",
+        "Level":"intermediate",
+       }
+    ],
+    "project":[
+        {
+            "title":"Auto indexing of gear cutting attachment in pnematic shaping machine",
+        }
+    ],
+
+}
+
+console.log(resume);
